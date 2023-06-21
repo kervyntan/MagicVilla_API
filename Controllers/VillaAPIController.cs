b156fb8463b862e56981e8597ac66de8770d1c15
Kervyn <73232587+kervyntan@users.noreply.github.com>
2023-06-21 14:00:36 +0800

    Added basic POST Method

--- Controllers/VillaAPIController.cs
+++ Controllers/VillaAPIController.cs
@@ -12,9 +12,9 @@ namespace MagicVilla_VillaAPI.Controllers
     public class VillaAPIController : ControllerBase
     {
         [HttpGet]
-        public IEnumerable<VillaDTO> GetVillas()
+        public ActionResult<IEnumerable<VillaDTO>> GetVillas()
         {
-            return VillaStore.villaList;
+            return Ok(VillaStore.villaList);
             //return new List<VillaDTO>
             //{
             //    new VillaDTO { Id = 1, Name="Pool Van"},
@@ -23,13 +23,25 @@ namespace MagicVilla_VillaAPI.Controllers
 
         // Getting Villa by Id
         // appending the placeholder for id to api/villa
+        // makes sure id passed in is required, and also of type int 
         [HttpGet("{id:int}")]
-        public VillaDTO GetVilla(int id)
+        public ActionResult<VillaDTO> GetVilla(int id)
         {
-            return VillaStore.villaList.FirstOrDefault
+            return Ok(VillaStore.villaList.FirstOrDefault
             (
                 u => u.Id == id
-            );
+            ));
+        }
+
+        // POST Method to update name of villa
+        [HttpPost("id")]
+        public ActionResult<VillaDTO> UpdateVillaName(int id, string updatedName)
+        {
+            VillaDTO entity = VillaStore.villaList.Find(x => x.Id == id);
+
+            entity.Name = updatedName;
+
+            return Ok(entity);
         }
     }
 }
