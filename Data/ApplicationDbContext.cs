2db5ff95473711481c17b82193789b8b6d9a884a
Kervyn <73232587+kervyntan@users.noreply.github.com>
2023-06-28 11:48:22 +0800

    Updated installed EntityFramework , created Db Context

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using MagicVilla_VillaAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MagicVilla_VillaAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        // Create individual dbset for each table
+
+        // Name is the name of the table in the database
+        public DbSet<Villa> Villas { get; set; }
+    }
+}
--- Models/Dtos/VillaDTO.cs
+++ Models/Dtos/VillaDTO.cs
@@ -9,5 +9,19 @@ namespace MagicVilla_VillaAPI.Models.Dtos
         [Required]
         [MaxLength(20)]
         public string Name { get; set; }
+
+        public string Details { get; set; }
+
+        [Required]
+        public double Rate { get; set; }
+
+        public int Sqft { get; set; }
+
+        public int Occupancy { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public string Amenity { get; set; }
+
     }
 }
