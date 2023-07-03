eeed8ff31965d22f56e5d7588c16839510396403
Kervyn <73232587+kervyntan@users.noreply.github.com>
2023-07-03 23:46:38 +0800

    Creating basic routes for Users

---
+++ Controllers/UserController.cs
@@ -0,0 +1,60 @@
+﻿using MagicVilla_VillaAPI.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_VillaAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UserController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetUsers()
+        {
+            return Ok(_context.Users.ToList());
+        }
+
+        [HttpGet("{id:int}")]
+        public IActionResult GetUserById(int id)
+        {
+            if(id > _context.Users.ToList().Count)
+            {
+                return BadRequest();
+            }
+
+            var response = _context.Users.FirstOrDefault(x => x.Id == id);
+
+            if (response == null)
+            {
+                return NotFound("User was not found.");
+            }
+
+            return Ok(response);
+        }
+
+        [HttpGet("{name}")]
+        public IActionResult GetUserByName(string name)
+        {
+            if (name == "")
+            {
+                return BadRequest();
+            }
+
+            var response = _context.Users.FirstOrDefault(x => x.Name == name);
+
+            if (response == null)
+            {
+                return NotFound("User was not found.");
+            }
+
+            return Ok(response);
+        }
+    }
+}
