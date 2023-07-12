d7c8981d5d7bfaff169b5d6adc81a40f5e704546
Kervyn <73232587+kervyntan@users.noreply.github.com>
2023-07-12 22:12:51 +0800

    Create VillaRepository

--- Repository/IRepository/IVillaRepository.cs
+++ Repository/IRepository/IVillaRepository.cs
@@ -7,10 +7,10 @@ namespace MagicVilla_VillaAPI.Repository.IRepository
     {
         // Filter to get the villa items that we want
         // Expression<Func<Villa>> is a filter/condition/linq statement
-        Task<List<Villa>> GetAll(Expression<Func<Villa>> filter = null);
+        Task<List<Villa>> GetAll(Expression<Func<Villa, bool>> filter = null);
 
         // For Patch -> AsNoTracking
-        Task<Villa> Get(Expression<Func<Villa>> filter = null, bool tracked=true);
+        Task<Villa> Get(Expression<Func<Villa, bool>> filter = null, bool tracked=true);
 
         // Creating a Villa
         Task Create(Villa entity);
---
+++ Repository/VillaRepository.cs
@@ -0,0 +1,67 @@
+﻿using MagicVilla_VillaAPI.Data;
+using MagicVilla_VillaAPI.Models;
+using MagicVilla_VillaAPI.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace MagicVilla_VillaAPI.Repository
+{
+    public class VillaRepository : IVillaRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public VillaRepository(ApplicationDbContext db)
+        {
+            _db = db;   
+        }
+        public async Task Create(Villa entity)
+        {
+            await _db.Villas.AddAsync(entity);
+            await SaveChanges();
+        }
+
+        public async Task<Villa> Get(Expression<Func<Villa, bool>> filter = null, bool tracked = true)
+        {
+            IQueryable<Villa> query = _db.Villas;
+
+            if (!tracked)
+            {
+                query = query.AsNoTracking();
+            }
+
+
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            return await query.FirstOrDefaultAsync();
+        }
+
+        public async Task<List<Villa>> GetAll(Expression<Func<Villa, bool>> filter = null)
+        {
+            // IQueryable does not exexcute right away
+            // can continue chaining methods to it
+            IQueryable<Villa> query = _db.Villas;
+
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            return await query.ToListAsync();
+        }
+
+        public async Task Remove(Villa entity)
+        {
+            _db.Villas.Remove(entity);
+            await SaveChanges();
+        }
+
+        public async Task SaveChanges()
+        {
+            await _db.SaveChangesAsync();
+        }
+    }
+}
